b5873b0d63cdc3bd18d9040875e7964d1682b2be
Dan-pach3co <danipg475@gmail.com>
2025-11-20 17:05:16 -0500

    second commit

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+#lista diccionario predeterminada 
+tasks = [
+    {"id": 1, 
+     "titulo": "Investigacion derechos humanos", 
+     "estado": "en_progreso", 
+     "fecha": "2025-11-25"},
+
+    {"id": 2, 
+     "titulo": "Mapa mental sobre variables aleatorias", 
+     "estado": "completada", 
+     "fecha": "2025-11-20"},
+
+    {"id": 3, 
+     "titulo": "Dashboard de productos", 
+     "estado": "pendiente", 
+     "fecha": "2025-11-30"},
+]
+next_id = 4
+
+@app.route("/")
+def task_list():
+    # Obtenemos el filtro de la URL (ej: /?estado=pendiente)
+    filtro = request.args.get('estado')
+    
+    if filtro and filtro != 'todas':
+        tareas_filtradas = [t for t in tasks if t["estado"] == filtro] #filtra por estado seleccionado si no son todos
+    else:
+        tareas_filtradas = tasks
+    return render_template("index.html", tasks=tareas_filtradas)
+
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    global next_id
+    titulo = request.form.get("titulo")
+    fecha = request.form.get("fecha")
+    
+    if titulo:
+        nueva_tarea = {
+            "id": next_id,
+            "titulo": titulo,
+            "estado": "pendiente", 
+            "fecha": fecha
+        }
+        tasks.append(nueva_tarea)
+        next_id += 1
+        
+    return redirect("/")
+
+@app.route("/delete/<int:task_id>")
+def delete_task(task_id):
+    global tasks
+    # Recreamos la lista excluyendo la tarea que queremos borrar
+    tasks = [task for task in tasks if task["id"] != task_id]
+    return redirect("/")
+
+
+@app.route("/edit/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    
+    tarea_a_editar = next((t for t in tasks if t["id"] == task_id), None) #busca la tarea
+    
+    if not tarea_a_editar:
+        return redirect("/")
+
+    if request.method == "POST":
+        tarea_a_editar['titulo'] = request.form.get('titulo')
+        tarea_a_editar['fecha'] = request.form.get('fecha')
+        tarea_a_editar['estado'] = request.form.get('estado')
+        return redirect("/") 
+    return render_template("index.html", tasks=tasks, task_to_edit=tarea_a_editar)
+
+
+@app.route("/detail/<int:task_id>")
+def view_detail(task_id):
+    tarea = next((t for t in tasks if t["id"] == task_id), None)
+    
+    # Si no existe la tarea, volvemos al inicio normal
+    if not tarea:
+        return redirect("/")
+    return render_template("index.html", detail=tarea, tasks=tasks)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
